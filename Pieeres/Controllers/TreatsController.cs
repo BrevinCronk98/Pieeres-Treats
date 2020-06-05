5dc06851606aae9476f4bc1989b3ffeae0533625
brevin <cronkbrevin98@gmail.com>
2020-06-05 10:26:02 -0700

    Treats Controller Complete, Work on Flavor Controller

---
+++ Pieeres/Controllers/TreatsController.cs
@@ -0,0 +1,80 @@
+using PieeresTreats.Models;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Threading.Tasks;
+
+namespace PieeresTreats.Controllers
+{
+    public class TreatsController : Controller
+    {
+        private readonly PieeresTreatsContext _db
+
+        public TreatsController(PieeresTreatsContext db)
+        {
+            _db = db;
+        }
+
+        public ActionResult Index()
+        {
+            List<Treat> model = _db.Treats.ToList();
+            return View(model);
+        }
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(Treat treat)
+        {
+            _db.Treats.Add(treat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Details(int id)
+        {
+            var thisTreat = _db.Treats
+                .Include(treat => treat.Flavors)
+                .ThenInclude(join => join.Flavor)
+                .FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
+
+        public ActionResult Edit()
+        {
+            var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id)
+            return View(thisTreat);
+        }
+
+        [HttpPost]
+        public ActionResult Edit(Treat treat)
+        {
+            _db.Entry(treat).State= EntityState.Modified;
+            _db.SaveChanges()
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Delete(int id)
+        {
+            var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            return View(thisTreat);
+        }
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            _db.Treats.Remove(thisTreat);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
