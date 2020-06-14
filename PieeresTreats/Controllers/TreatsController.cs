5cce327c7143ea2dd8f14eb11bd8fab6034ff357
brevin <cronkbrevin98@gmail.com>
2020-06-14 14:27:37 -0700

    I think the MtM relationships are working, I am getting a foreign key constraint fail

--- PieeresTreats/Controllers/TreatsController.cs
+++ PieeresTreats/Controllers/TreatsController.cs
@@ -69,21 +69,21 @@ namespace PieeresTreats.Controllers
             }
             _db.Entry(treat).State= EntityState.Modified;
             _db.SaveChanges();
-            return RedirectToAction("Details", new {id = treat.TreatId});
+            return RedirectToAction("Index");
         }
 
 
         [Authorize]
         public ActionResult Delete(int id)
         {
-            var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
             return View(thisTreat);
         }
 
         [HttpPost, ActionName("Delete")]
         public ActionResult DeleteConfirmed(int id)
         {
-            var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
             _db.Treats.Remove(thisTreat);
             _db.SaveChanges();
             return RedirectToAction("Index");
@@ -92,20 +92,21 @@ namespace PieeresTreats.Controllers
         [Authorize]
         public ActionResult AddFlavor(int id)
         {
-            var thisTreat = _db.Treats.FirstOrDefault(treat => treat.TreatId == id);
+            var thisTreat = _db.Treats.FirstOrDefault(treats => treats.TreatId == id);
             ViewBag.FlavorId = new SelectList(_db.Flavors, "FlavorId", "Name");
             return View(thisTreat);
         }
 
         [HttpPost]
-        public ActionResult AddFlavor(Treat Treat, int FlavorId)
+        public ActionResult AddFlavor(Treat treat, int FlavorId)
         {
             if(FlavorId != 0)
             {
-                _db.TreatFlavors.Add(new TreatFlavor() {FlavorId = FlavorId, TreatId = Treat.TreatId});
+                _db.TreatFlavors.Add(new TreatFlavor() {FlavorId = FlavorId, TreatId = treat.TreatId});
             }
+            _db.Entry(treat).Collection(r => r.Flavors).IsModified = true;
             _db.SaveChanges();
-            return RedirectToAction("Details", new { id = Treat.TreatId });
+            return RedirectToAction("Index");
         }
     }
 }
--- PieeresTreats/Models/PieeresTreatsContext.cs
+++ PieeresTreats/Models/PieeresTreatsContext.cs
@@ -5,8 +5,8 @@ namespace PieeresTreats.Models
 {
     public class PieeresTreatsContext : IdentityDbContext<ApplicationUser>
     {
-        public virtual DbSet<Treat> Treats{ get; set; }
-        public DbSet<Flavor> Flavors{ get; set; }
+        public DbSet<Treat> Treats{ get; set; }
+        public virtual DbSet<Flavor> Flavors{ get; set; }
         public DbSet<TreatFlavor> TreatFlavors { get; set; }
 
         public PieeresTreatsContext(DbContextOptions options) : base(options) { }
